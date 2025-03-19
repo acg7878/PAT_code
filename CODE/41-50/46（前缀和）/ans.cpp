dc5190c27c1d3036a802972e912d53bf8d3b41cd
macbook_air <2477952281@qq.com>
2025-03-19 08:54:11 +0800

    3.19

---
+++ CODE/41-50/46（前缀和）/ans.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n, m;
+    std::cin >> n;
+    std::vector<int> exits(n + 1);
+    std::vector<int> prefix_sum(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        std::cin >> exits[i];
+        prefix_sum[i] = prefix_sum[i - 1] + exits[i];
+    }
+
+    int total_sum = prefix_sum[n];
+    std::cin >> m;
+    while (m--) {
+        int u1, u2;
+        std::cin >> u1 >> u2;
+        if (u1 > u2) std::swap(u1, u2);
+        int dist1 = prefix_sum[u2 - 1] - prefix_sum[u1 - 1];
+        int dist2 = total_sum - dist1;
+        std::cout << std::min(dist1, dist2) << std::endl;
+    }
+}
