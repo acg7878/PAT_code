d3dbedda7227faa78c65ed366778cd43f4484e21
macbook_air <2477952281@qq.com>
2025-03-21 08:32:36 +0800

    3.21

---
+++ CODE/41-50/48（双指针）/my.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+int main() {
+    int N, M;
+    std::cin >> N >> M;
+    std::vector<int> coins(N);
+    for (int i = 0; i < N; i++) {
+        std::cin >> coins[i];
+    }
+    std::sort(coins.begin(),coins.end());
+    int left = 0, right = N - 1;
+    while (left < right) {
+        int sum = coins[left] + coins[right];
+        if (sum == M) {
+            std::cout << coins[left] << " " << coins[right] << std::endl;
+            return 0;
+        } else if (sum > M)
+            right--;
+        else
+            left++;
+    }
+    std::cout << "No Solution" << std::endl;
+}
